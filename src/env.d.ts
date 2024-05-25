e712ffb5c65711d0aa0ca0b1b9aa3ea734bcdcbf
null <keita@gaogao.asia>
2024-05-26 04:14:07 +0900

    ✨ add form #2

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
+/// <reference path="../.astro/actions.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly NOTION_TOKEN: string;
+  readonly DB_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
